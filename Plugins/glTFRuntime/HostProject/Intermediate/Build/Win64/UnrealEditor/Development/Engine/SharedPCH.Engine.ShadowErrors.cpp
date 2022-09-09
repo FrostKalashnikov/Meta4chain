a56e0cf4974577ed2787f2970e468f6dcf3909f1
FrostKalashnikov <44955473+FrostKalashnikov@users.noreply.github.com>
2022-09-09 14:26:57 -0300

    fix ready player me material

--- Plugins/glTFRuntime/HostProject/Intermediate/Build/Win64/UnrealEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30140
-#include "D:/Users/Downloads/Unreal UE5 RPM Plugin/glTFRuntime/HostProject/Intermediate/Build/Win64/UnrealEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Plugins/glTFRuntime/HostProject/Intermediate/Build/Win64/UnrealEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30140
-#include "D:/Users/Downloads/Unreal UE5 RPM Plugin/glTFRuntime/HostProject/Intermediate/Build/Win64/UnrealEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.h"
